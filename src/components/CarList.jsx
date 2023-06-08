a46389ee2a54842280591291d755e7cf9d33e15e
Felps <fe.thomaz7@gmail.com>
2023-06-08 20:29:55 -0300

    Listing records and displaying to the user

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,15 +1,36 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { render } from 'react-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCar } from '../store/slices/carsSlice';
 
 const CarList = () => {
+  const dispatch = useDispatch();
   const cars = useSelector((state) => {
     // access the big State -> access to store (cars) store/index.jsx -> access data key inside carsSlice.js
     return state.cars.data;
   });
 
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id));
+  };
+
+  const renderedCars = cars.map((car) => {
+    return(
+      <div key={car.id} className='panel'>
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button className='button is-danger'>Delete</button>
+      </div>
+    )
+  })
+
   return (
-    <div></div>
+    <div className='car-list'>
+      {renderedCars}
+      <hr />
+    </div>
   )
 }
 
