1b0051dc8958a1c889c3a043cf13a43273645567
Felps <fe.thomaz7@gmail.com>
2023-06-08 20:12:12 -0300

    Fixing awkward double keys coming from the carsSlice initialState key

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const CarList = () => {
+  const cars = useSelector((state) => {
+    // access the big State -> access to store (cars) store/index.jsx -> access data key inside carsSlice.js
+    return state.cars.data;
+  });
+
   return (
-    <div>CarList</div>
+    <div></div>
   )
 }
 
-export default CarList
+export default CarList;
